a6afd27937ab1d500549386d41a28cad1f1baf4f
sumitinstates <ss1635@jagmail.southalabama.edu>
2019-05-23 18:52:05 -0400

    FirstMVC code

---
+++ src/com/sumit/HelloworldController.java
@@ -0,0 +1,19 @@
+package com.sumit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/")
+public class HelloworldController {
+
+
+    @RequestMapping(method=RequestMethod.GET)
+	public ModelAndView welcome() {
+		return new ModelAndView("WelcomePage", "WelcomeMessage", "welcome to MVC world");
+		
+		
+	}
+}
